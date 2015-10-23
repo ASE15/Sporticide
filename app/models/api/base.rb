17860b0e6d6cd6dc484e6cd6c51163ef49feb133
lukasvonrotz <lukas.v.rotz@hotmail.com>
2015-10-23 13:18:20 +0200

    adjusted api base

--- app/models/api/base.rb
+++ app/models/api/base.rb
@@ -19,44 +19,6 @@ class Api::Base < ActiveResource::Base
   self.format = :json
 
 
-  # Strip .xml extension off generated URLs
-  ##################################
-  ##  Take either this approach    #
-  ##################################
-  #class << self
-  #  def element_path(id, prefix_options = {}, query_options = nil)
-  #    check_prefix_options(prefix_options)
-  #    prefix_options, query_options = split_options(prefix_options) if query_options.nil?
-  #    "#{prefix(prefix_options)}#{collection_name}/#{URI.parser.escape id.to_s}#{query_string(query_options)}"
-  #  end
-  #
-  #  def collection_path(prefix_options = {}, query_options = nil)
-  #    check_prefix_options(prefix_options)
-  #    prefix_options, query_options = split_options(prefix_options) if query_options.nil?
-  #    "#{prefix(prefix_options)}#{collection_name}#{query_string(query_options)}"
-  #  end
-  #
-  #  def instantiate_collection(collection, prefix_options = {}, b = nil)
-  #    #collection = collection[element_name.pluralize] if collection.instance_of?(Hash)
-  #    collection = collection['listOfPatient']['patient'] if collection.instance_of?(Hash)
-  #    #collection = JSON.parse(collection)['listOfPatient']
-  #    collection.collect! { |record| instantiate_record(record, prefix_options) }
-  #  end
-  #
-  #  def create
-  #    run_callbacks :create do
-  #      connection.put(collection_path, encode, self.class.headers).tap do |response|
-  #        self.id = id_from_response(response)
-  #        load_attributes_from_response(response)
-  #      end
-  #    end
-  #  end
-  #end
-
-  #################################
-  #     Or take this approach     #
-  #################################
-
   # that activeresource doesnt send https://api.example.com/contracts.xml or .json, but https://api.example.com/contracts
   class << self
     def element_path(id, prefix_options = {}, query_options = nil)
@@ -70,11 +32,5 @@ class Api::Base < ActiveResource::Base
     def new_element_path
       super.gsub(/.json|.xml/,'')
     end
-    #def instantiate_collection(collection, prefix_options = {}, b = nil)
-    #  #collection = collection[element_name.pluralize] if collection.instance_of?(Hash)
-    #  collection = collection['listOfPatient']['patient'] if collection.instance_of?(Hash)
-    #  collection.collect! { |record| instantiate_record(record, prefix_options) }
-    #end
-
   end
 end
