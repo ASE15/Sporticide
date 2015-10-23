9b8ef5b94fe104ef467bdfc5fc0bbb12e54aa81b
Lafunamor <info@zipfuzapfu.ch>
2015-10-23 13:46:21 +0200

    basic user model and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,4 +30,17 @@ class ApplicationController < ActionController::Base
       redirect_to @training
     end
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+  def user_signed_in?
+    unless current_user == nil
+      return false
+    end
+    return true
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,5 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-
+require 'active_resource'
+class User  < Api::Base
   #has_many :trainings, foreign_key: "owner_id"
   has_and_belongs_to_many :trainings
 
@@ -15,5 +10,40 @@ class User < ActiveRecord::Base
 
 
   has_many :logs
+
+
+  self.prefix = '/CyberCoachServer/resources/'
+
+  #self.user = "newuser1"
+  #self.password = "newuser"
+
+  # To handle the issue with getting the resources by name (e.g. ../resources/sports/Running not ../sports/1)
+  # you can set the primary_key to the created api class extending the ActiveResource::Base class.
+  self.primary_key = 'username'
+
+  # singular name of the resource. you only need to specify this if this class name is not the resource name
+  self.element_name = 'user'
+
+  class << self
+    def instantiate_collection(collection, prefix_options = {}, b = nil)
+      collection = collection['users'] if collection.instance_of?(Hash)
+      collection.collect! { |record| instantiate_record(record, prefix_options) }
+    end
+  end
+
+  #schema do
+  #  string 'uri', 'username', 'password', 'realname', 'email', 'subscriptions', 'partnerships'
+  #  integer 'publicvisible'
+  #  end
+
+  #validates :lastName,  :presence => true, :length => { :maximum => 50 }
+  #validates :firstName, :presence => true, :length => { :minimum => 6 }
+
+
+
+ def self.user_signed_in?
+   return false
+ end
+
 
 end
