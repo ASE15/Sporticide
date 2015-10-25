42e38bc8e15278547b749fcc96321a9e08d1bef7
Pas <pascal.zaugg@students.unibe.ch>
2015-10-25 11:48:31 +0100

    Changed environment for testing reasons.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+begin
+  klass = Module.const_get("Active::Resource")
+  puts klass
+  klass.is_a?(Class)
+rescue NameError
+  puts "Active::Resource does not exist!"
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
