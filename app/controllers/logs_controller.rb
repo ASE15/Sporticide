7fe310f3b2f307aed502041ac8e3348891b30f43
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2015-10-26 16:28:58 +0100

    fixed wrong human thinking

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -28,6 +28,8 @@ class LogsController < ApplicationController
       @log = @session.logs.create(log_params)
       @log.user = current_user
       if @log.save
+        create_cc_entry(current_user, @session.training.sport, @session, @log)
+
         redirect_to @session.training, :notice => 'Log created.'
       else
         render 'new'
@@ -54,5 +56,26 @@ class LogsController < ApplicationController
   private
   def log_params
     params.require(:log).permit(:intensity, :rating, :comment)
+  end
+
+  def create_cc_entry(user, sport, session, log)
+    cc_user = 'andi'
+    cc_pass = 'test'
+    #ToDo insert real username
+    begin
+      digest = Base64.encode64(cc_user+':'+cc_pass)
+      result = RestClient.post 'http://diufvm31.unifr.ch:8090/CyberCoachServer/resources/users/'+cc_user+'/'+sport,
+                               :entrylocation => session.location,
+                               :entryduration => session.duration,
+                               :entrydate => session.datetime,
+                               :comment => log.comment,
+                               :publicvisible => 1,
+                               :Authorization => 'Basic '+digest
+      #ToDo add parameters
+      doc = Nokogiri::XML(result)
+
+    rescue Exception => e
+      status=false
+    end
   end
 end
