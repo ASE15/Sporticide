52f8f5ee2a7fe29edae7bdccb6e01b4523bdabcc
Lafunamor <info@zipfuzapfu.ch>
2015-10-27 09:58:59 +0100

    merged ccuserintegration into master deleted old FB / Devise files

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-begin
-  klass = Module.const_get("Active::Resource")
-  puts klass
-  klass.is_a?(Class)
-rescue NameError
-  puts "Active::Resource does not exist!"
-end
 
 # Initialize the Rails application.
 Rails.application.initialize!
