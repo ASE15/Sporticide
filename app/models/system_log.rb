3d01fdfa9a7ac1fd269bbc3ac061bfdf1d1b039e
Karan <karan.d.sethi@gmail.com>
2015-11-22 14:09:07 +0100

    notification saving

--- app/models/system_log.rb
+++ app/models/system_log.rb
@@ -1,4 +1,4 @@
 class SystemLog < ActiveRecord::Base
   belongs_to :training_session
-  has_many :training_notifiers, :inverse_of => :system_log, :dependent => :destroy
+  # has_many :training_notifiers, :inverse_of => :system_log, :dependent => :destroy
 end
--- db/migrate/20151122105451_create_training_notifiers.rb
+++ db/migrate/20151122105451_create_training_notifiers.rb
@@ -1,9 +1,9 @@
 class CreateTrainingNotifiers < ActiveRecord::Migration
   def change
     create_table :training_notifiers do |t|
+      t.isRead :boolean
       t.system_log_id :integer
       t.local_user_id :integer
-      t.isRead :boolean
 
       t.timestamps null: false
     end
