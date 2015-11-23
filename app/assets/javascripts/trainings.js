51a32e205735f060ab47c07d86a1499e269d445e
Pas <pascal_zaugg@web.de>
2015-11-23 11:35:55 +0100

    Added more search funtionality. You can now search for locations. Added another column to the trainings to show the locations of a training.

--- app/assets/javascripts/trainings.js
+++ app/assets/javascripts/trainings.js
@@ -10,9 +10,26 @@ jQuery(function(){
               text;
 
           $rows.show().filter(function() {
-              text = $(this).find(".sport").text().replace(/\s+/g, ' ');
+              text = $(this).find(".sport-entry").text().replace(/\s+/g, ' ');
               return !reg.test(text);
           }).hide();
     }
   });
+  
+  $('.location').change(function() {
+      if($(this).val() == "all") {
+          $rows.show();
+      }
+      else {
+          var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+              reg = RegExp(val, 'i'),
+              text;
+
+          $rows.show().filter(function() {
+              text = $(this).find(".location-entry").text().replace(/\s+/g, ' ');
+              return !reg.test(text);
+          }).hide();
+    }
+  });
+  
 });
--- app/models/training.rb
+++ app/models/training.rb
@@ -3,7 +3,6 @@ class Training < ActiveRecord::Base
   has_many :training_sessions, :inverse_of => :training, :dependent => :destroy
   has_many :comments, :inverse_of => :training, :dependent => :destroy
 
-
   #has_and_belongs_to_many :users
 
   has_and_belongs_to_many(:users,
@@ -16,4 +15,29 @@ class Training < ActiveRecord::Base
   #                        :join_table => "members_trainings",
   #                        :foreign_key => "training_id",
   #                        :association_foreign_key => "user_id")
+  
+  def self.get_all_locations()
+    locations = []
+    
+    @relation.each do |training| 
+      training.training_sessions.each do |session|
+        if not session.location.nil? and not session.location.empty? and not locations.include?(session.location)
+          locations.push(session.location)
+        end
+      end
+    end
+    
+    return locations
+  end
+  
+  def get_locations()
+    locations = []
+    training_sessions.each do |session|
+        if not session.location.nil? and not session.location.empty? and not locations.include?(session.location)
+          locations.push(session.location)
+        end
+    end
+    
+    return locations
+  end
 end
