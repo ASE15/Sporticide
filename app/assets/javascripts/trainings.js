161a9b491c5c3f03f9f633c1b9dc908e63407836
Pas <pascal_zaugg@web.de>
2015-11-23 17:37:52 +0100

    Combined range search for date now with all the others. Therefore finished search now!

--- app/assets/javascripts/trainings.js
+++ app/assets/javascripts/trainings.js
@@ -4,6 +4,36 @@ jQuery(function(){
       show();
   });
   
+  $('.location').change(function() {
+     show();
+  });
+  
+  $('.recurrence').change(function() {
+     show();
+  });
+  
+  $("#start-date-input").datepicker({
+        onSelect: function(date) {
+          show();
+        },
+      });
+      
+  $("#end-date-input").datepicker({
+        onSelect: function(date) {
+          show();
+        },
+      });
+  
+  $("#start-date-clear").click(function(){
+    $("#start-date-input").val('');
+    show();
+  });
+  
+  $("#end-date-clear").click(function(){
+    $("#end-date-input").val('');
+    show();
+  });
+  
   function show(line) {
       var t_type_reg = reg_exp('.training-type');
       var t_type_all = is_all('.training-type');
@@ -15,7 +45,8 @@ jQuery(function(){
       $rows.show().filter(function() {
           return filter(this, t_type_all, t_type_reg, ".sport-entry") 
           || filter(this, location_all, location_reg, ".location-entry") 
-          || filter(this, recurrence_all, recurrence_reg, ".recurrence-entry") ;
+          || filter(this, recurrence_all, recurrence_reg, ".recurrence-entry")
+          || date_filter(this);
       }).hide();
   }
   
@@ -33,64 +64,21 @@ jQuery(function(){
       return RegExp(val, 'i');
   }
   
-  $('.location').change(function() {
-     show();
-  });
-  
-  $('.recurrence').change(function() {
-     show();
-  });
-  
-  $("#start-date-input").datepicker({
-        onSelect: function(date) {
-          $rows.show().filter(function() {
-              var start_date = new Date($(this).find(".start-entry").attr("data-date"));
-              var selected_start_date = $.datepicker.parseDate( "mm/dd/yy", date);
-              var selected_end_date = $("#end-date-input").datepicker("getDate");
-              start_date.setMinutes(start_date.getMinutes()+selected_start_date.getTimezoneOffset());
-              
-              var shouldShow = true
-              if(selected_start_date && !isNaN(selected_start_date.getTime())) {
-                shouldShow = shouldShow && selected_start_date <= start_date;
-              }
-              
-              if(selected_end_date && !isNaN(selected_end_date.getTime())) {
-                shouldShow = shouldShow && selected_end_date >= start_date
-              }
-              
-              return !shouldShow;
-          }).hide();
-        },
-      });
-      
-  $("#end-date-input").datepicker({
-        onSelect: function(date) {
-          $rows.show().filter(function() {
-              var selected_end_date = $("#end-date-input").datepicker("getDate");
-              var selected_start_date = $("#start-date-input").datepicker("getDate");
-              var start_date = new Date($(this).find(".start-entry").attr("data-date"));
-              start_date.setMinutes(start_date.getMinutes()+selected_start_date.getTimezoneOffset());
-              
-              var shouldShow = true
-              if(selected_start_date && !isNaN(selected_start_date.getTime())) {
-                shouldShow = shouldShow && selected_start_date <= start_date;
-              }
-              
-              if(selected_end_date && !isNaN(selected_end_date.getTime())) {
-                shouldShow = shouldShow && selected_end_date >= start_date
-              }
-              
-              return !shouldShow;
-          }).hide();
-        },
-      });
-  
-  $("#start-date-clear").click(function(){
-    $rows.show()
-    $("#start-date-input").val('');
-  });
-  
-  $("#end-date-clear").click(function(){
-    $("#end-date-input").val('');
-  });
+  function date_filter(line) {
+    var start_date = new Date($(line).find(".start-entry").attr("data-date"));
+    var selected_end_date = $("#end-date-input").datepicker("getDate");
+    var selected_start_date = $("#start-date-input").datepicker("getDate");
+    start_date.setMinutes(start_date.getMinutes()+selected_start_date.getTimezoneOffset());
+    
+    var shouldShow = true
+    if(selected_start_date && !isNaN(selected_start_date.getTime())) {
+      shouldShow = shouldShow && selected_start_date <= start_date;
+    }
+    
+    if(selected_end_date && !isNaN(selected_end_date.getTime())) {
+      shouldShow = shouldShow && selected_end_date >= start_date
+    }
+    
+    return !shouldShow
+  }
 });
