d73a346a2b17e37fc6aa91a755da8ce7341438b8
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2015-12-01 12:55:18 +0100

    log added comment validation

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,6 @@
 class Log < ActiveRecord::Base
   belongs_to :training_session, :inverse_of => :logs
   belongs_to :user, class_name: 'LocalUser', :inverse_of => :logs
+
+  validates :comment, :length => { :minimum => 1 }, allow_nil: false, allow_blank: false
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :user, class_name: "LocalUser"
   belongs_to :chat, :inverse_of => :messages
+
+
+  validates :text, :length => { :minimum => 1 }, allow_nil: false, allow_blank: false
 end
